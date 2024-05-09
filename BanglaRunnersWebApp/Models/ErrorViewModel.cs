a9cbaca36fee72f7d7db992c4c00ed9209bae56e
Anam Mofazzal Durjoy <durjoy6405@gmail.com>
2024-05-10 02:27:55 +0600

    Add project files.

---
+++ BanglaRunnersWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BanglaRunnersWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
