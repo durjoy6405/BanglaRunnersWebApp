804cb38ee0d3950da44a596682bde5d51d88978b
Anam Mofazzal Durjoy <durjoy6405@gmail.com>
2024-05-18 23:58:16 +0600

    New code is added

--- BanglaRunnersWebApp/Models/AppUser.cs
+++ BanglaRunnersWebApp/Models/AppUser.cs
@@ -1,7 +1,11 @@
-﻿namespace BanglaRunnersWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BanglaRunnersWebApp.Models
 {
     public class AppUser
     {
+        [Key]
+        public string id { get; set; }
         public int? Pace { get; set; }
         public int? Mileage { get; set; }
         public Address? Address { get; set; }
